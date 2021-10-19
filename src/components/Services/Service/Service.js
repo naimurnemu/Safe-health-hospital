299bc18b860e706eb2fef1c843320ef3c11eb769
naimurnemu <naimurrahaman82@gmail.com>
2021-10-19 14:46:06 +0600

    service design done

--- src/components/Services/Service/Service.js
+++ src/components/Services/Service/Service.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Service = ({service}) => {
-  const {id, title, price, unit, description, img} = service
+const Service = ({ service }) => {
+  const { id, title, price, description, img } = service;
   return (
-    <div>
-      <h2>This is a single service</h2>
+    <div className="col text-dark">
+      <div className="card">
+        <img src={img} className="img-fluid rounded" alt="service-img" />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <div className="text-start">
+            <h4>Cost: ${price}</h4>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+        <div>
+          <Link><button className="btn btn-primary m-2">More Details</button></Link>
+        </div>
+      </div>
     </div>
   );
 };
